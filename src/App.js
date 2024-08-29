d27ed5258a0629b8fc4c4efb278ca68a01fc6bb7
Josh Stafford <joshuaostafford@gmail.com>
2024-08-29 14:06:45 -0700

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import InputForm from './components/InputForm';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';  // Import the App.css file
-
+import React, { useEffect } from 'react';
+import { initGA, logPageView } from './analytics';
 
 function App() {
+
+  useEffect(() => {
+    initGA();
+    logPageView();
+}, []);
+
   return (
     <div className="App">
       <InputForm />
